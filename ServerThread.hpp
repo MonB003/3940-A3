be1c4bd1542a970d4af8487219dbda2498d78fe4
Brad <bmasciotra@my.bcit.ca>
2022-11-04 17:11:04 -0700

    serverthread cpp working

--- ServerThread.hpp
+++ ServerThread.hpp
@@ -15,22 +15,16 @@ class ServerThread : public Thread
 {
 
 public:
-    //Response res;
-    //Request req;
-    ServerThread(int msgSock) : Thread(this)
-    {
-        cout << "creating server thread" << endl;
-        this->msgsocket = msgSock;
-    };
-
-    // get ---- request class
-    void get(){
-        cout <<"get request called"<< endl;
-    };
+     ServerThread(int msgSock);
+  
+   
+    // void get(){
+    //     cout <<"get request called"<< endl;
+    // };
 
     // post ---- request class
-    void post()
-    {
+    // void post()
+    // {
         
 
     //     char arr[1024] = "HTTP/1.1 200 OK\r\n\r\n<html>\n\r<body>\n\r\rhello\n\r</body>\n</html>";
@@ -46,60 +40,60 @@ public:
     //     {
     //         cout << "send success\n";
     //     }
-    }
+    // }
 
-    int renderHTML()
-    {
+    int renderHTML();
+    // {
 
-        string html = "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 600\n\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title> File Upload Form</title>\r\n</head>\r\n<body>\r\n<h1>Upload file</h1>\r\n<form id =\"form\" method=\"POST\" action=\"/\" enctype=\"multipart/form-data\">\r\n<input type=\"file\" name=\"fileName\"/><br/><br/>\r\nCaption: <input type =\"text\" name=\"caption\"<br/><br/>\r\n <br/>\nDate : <input type=\"date\" name=\"date\"<br/><br/>\r\n <br/>\n <input id='formBtn' type=\"submit\" name=\"submit\" value=\"Submit\"/>\r\n </form>\r\n</body>\r\n</html>\r\n";
+    //     string html = "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 600\n\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title> File Upload Form</title>\r\n</head>\r\n<body>\r\n<h1>Upload file</h1>\r\n<form id =\"form\" method=\"POST\" action=\"/\" enctype=\"multipart/form-data\">\r\n<input type=\"file\" name=\"fileName\"/><br/><br/>\r\nCaption: <input type =\"text\" name=\"caption\"<br/><br/>\r\n <br/>\nDate : <input type=\"date\" name=\"date\"<br/><br/>\r\n <br/>\n <input id='formBtn' type=\"submit\" name=\"submit\" value=\"Submit\"/>\r\n </form>\r\n</body>\r\n</html>\r\n";
 
-        char arr[200] = "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 16\n\n<h1>testing</h1>";
+    //     char arr[200] = "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 16\n\n<h1>testing</h1>";
 
 
-       int send_res = send(msgsocket, html.c_str(), html.length(), 0); // 
-        // do a response...
-        return send_res;
-    }
+    //    int send_res = send(msgsocket, html.c_str(), html.length(), 0); // 
+    //     // do a response...
+    //     return send_res;
+    // }
 
     // display html
 
     void parseRequest(){};
 
-    void run()
-    {
+    void run();
+    // {
 
-       // string get_http = "GET / HTTP/1.1\r\nHost: " + url + "\r\nConnection: close\r\n\r\n";
+    //    // string get_http = "GET / HTTP/1.1\r\nHost: " + url + "\r\nConnection: close\r\n\r\n";
 
-       // res = new Response(msgsocket); // socket stream in
+    //    // res = new Response(msgsocket); // socket stream in
 
-        char buffer[1024*1024];
+    //     char buffer[1024*1024];
 
-        recv(msgsocket, &buffer, 1024*1024,0);
+    //     recv(msgsocket, &buffer, 1024*1024,0);
 
-        // istringstream requestInfo = buffer.c_str();
-        istringstream requestInfo(buffer);
-        istringstream *reqPtr = &requestInfo;
-        cout << buffer <<endl;
-        Request *req = new Request(reqPtr);
+    //     // istringstream requestInfo = buffer.c_str();
+    //     istringstream requestInfo(buffer);
+    //     istringstream *reqPtr = &requestInfo;
+    //     cout << buffer <<endl;
+        
+    //     Request *req = new Request(reqPtr);
 
        
 
-        //send(msgsocket,get_http, strlen(get_http.c_str()),0 );
+    //     //send(msgsocket,get_http, strlen(get_http.c_str()),0 );
 
-        cout << "here in run method" << endl;
-        // we should distinguish between get and post here.....
-        int res = renderHTML();
-        cout << res <<endl;
-
-
+    //     cout << "here in run method" << endl;
+    //     // we should distinguish between get and post here.....
+    //     int res = renderHTML();
+    //     cout << res <<endl;
 
 
 
 
 
 
-    };
-    ~ServerThread();
+
+    // };
+    ~ServerThread() = default;
 
 private:
     int msgsocket; // socket reference.
